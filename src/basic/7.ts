07fdbb4ac7c6b1e35c24efc62085c6509b37807e
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-06-22 21:34:47 +0200

    fix from comment

--- src/basic/7.ts
+++ src/basic/7.ts
@@ -2,13 +2,12 @@ interface Page {
   title: string;
   likes: number;
   accounts: string[];
-  status: string;
+  status: "open" | "close";
   details?: {
     createAt: Date;
     updateAt: Date;
   };
 }
-
 const page1: Page = {
   title: "The awesome page",
   likes: 100,
